22a79101b9a8a188fd5c1e349da46e25e16ffec8
bastien <bvtbastien@gmail.com>
2020-06-20 11:41:56 +0200

    Modification Footer

--- footer.php
+++ footer.php
@@ -3,7 +3,24 @@
 <footer>
 		<div class="wrapper footer">
 			<ul>
+				<li class="links">
+					<?php
+					wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
 
+					?>
+				</li>
+				<li class="links">
+					<ul>
+						<li><a href="http://ensim.univ-lemans.fr/fr/index.html">ENSIM</a></li>
+
+					</ul>
+				</li>
+				<li class="links">
+					<ul>
+						<li><a href="http://www.univ-lemans.fr/fr/index.html">Université du Mans</a></li>
+					</ul>
+					
+				</li>
 				<li class="about">
 					<p>Ce thème a été créer dans le cadre du cours système de gestion de contenue en 4A ips à L'ensim</p>
 					<ul>
